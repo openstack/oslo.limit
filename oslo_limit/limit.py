openstack/oslo.limit

commit 4a3c10b58ec8ed64756c06876e67928b49595e7a

Added ability to select identity interface
Extent configuration options to allow communicating with identity
over interface different than public.

Closes-Bug: #2105987
Change-Id: I3c2cb65337afc2cddd2a4771545139d470540a06

oslo_limit/limit.py
@@ -40,7 +40,10 @@ def _get_keystone_connection():
                 CONF, group='oslo_limit')
             session = loading.load_session_from_conf_options(
                 CONF, group='oslo_limit', auth=auth)
-            _SDK_CONNECTION = connection.Connection(session=session).identity
+            _SDK_CONNECTION = connection.Connection(
+                session=session,
+                interface=CONF.oslo_limit.interface
+            ).identity
         except (ksa_exceptions.NoMatchingPlugin,
                 ksa_exceptions.MissingRequiredOptions,
                 ksa_exceptions.MissingAuthPlugin,

oslo_limit/opts.py
@@ -43,6 +43,11 @@ _options = [
         choices=['public', 'publicURL', 'internal', 'internalURL',
                  'admin', 'adminURL'],
         help=_("The interface for endpoint discovery")),
+    cfg.StrOpt(
+        'interface',
+        default='public',
+        choices=['public', 'internal', 'admin'],
+        help=_("The interface used for communication with identity service.")),
 ]
 
 _option_group = 'oslo_limit'

releasenotes/notes/ability-to-select-interface-for-obtaining-limits-bae308e3d2f515f0.yaml
@@ -1,0 +1,6 @@
+---
+features:
+  - |
+    Added option ``interface`` to ``[oslo_limit]`` section, introducing ability
+    to select interface for communicating with identity service for obtaining
+    limits.

commit 327977e0991fb292f4de1939f821958d1e8ff0c9

Fix ignored KSA adapter options
KSA adapter options have been registered but these have been unused.
Fix the ignored adapter options so that a few important options such
as valid_interfaces work.

Note that we can't use the native oslo_conf argument because
the current behavior of Connection class attempts to load these adapter
options from the "keystone" section, while there are actually
registered in "oslo_limits" section.

This effectively reverts 4a3c10b58ec8ed64756c06876e67928b49595e7a
because the same functionality can be now achieved by
the valid_interface option.

Conflicts:
	oslo_limit/limit.py

Resolved conflict caused by 4a3c10b58ec8ed64756c06876e67928b49595e7a
which is not present in stable branches.

Closes-Bug: 2105987
Change-Id: I038b805be85b9b16c8a1a9eeaa1c90f8f413cc97
(cherry picked from commit f351266cba76cf9aa7791432611088134edaf943)

oslo_limit/limit.py
@@ -40,7 +40,19 @@ def _get_keystone_connection():
                 CONF, group='oslo_limit')
             session = loading.load_session_from_conf_options(
                 CONF, group='oslo_limit', auth=auth)
-            _SDK_CONNECTION = connection.Connection(session=session).identity
+            ksa_opts = loading.get_adapter_conf_options(
+                include_deprecated=False)
+            conn_kwargs = {}
+            for opt in ksa_opts:
+                if opt.dest != 'valid_interfaces':
+                    conn_kwargs['identity_' + opt.dest] = getattr(
+                        CONF.oslo_limit, opt.dest)
+            conn_kwargs['identity_interface'] = \
+                CONF.oslo_limit.valid_interfaces
+            _SDK_CONNECTION = connection.Connection(
+                session=session,
+                **conn_kwargs
+            ).identity
         except (ksa_exceptions.NoMatchingPlugin,
                 ksa_exceptions.MissingRequiredOptions,
                 ksa_exceptions.MissingAuthPlugin,

releasenotes/notes/fix-ignored-ksa-adapter-options-e120fac9a6fd35f7.yaml
@@ -1,0 +1,5 @@
+---
+fixes:
+  - |
+    Fix ignored keystoneauth adapter options such as valid_interfaces.
+